4c96fa389deb54c2a6cde43d9264daed31efb449
bruktawit mekuria <buktawit0101@gmail.com>
2023-08-28 09:25:20 -0700

    add a new statment

--- hellow_world.py
+++ hellow_world.py
@@ -3,4 +3,4 @@ print("Hello world from GitHub")
 
 print("I am a basic script leaving in GitHub typing some text")
 
-print("I just added a new line in a file")
+print("I just added a new line in a file !")
